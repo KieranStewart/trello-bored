f52d55afebb84df9198c2f3a46b21d120cb50a31
KieranStewart <kieranmstewart@gmail.com>
2026-02-11 16:49:58 -0500

    Updated info and created template

---
+++ bored-api/bored-api.py
@@ -0,0 +1,27 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return Response('Hello World!', 200)
+
+@app.route('/review')
+def review(): # TODO Write this endpoint (review)
+    return Response('Default Response', 200)
+
+@app.route('/merge')
+def merge(): # TODO Write this endpoint (merge)
+    return Response('Default Response', 200)
+
+@app.route('/pr')
+def pr(): # TODO Write this endpoint (pr)
+    return Response('Default Response', 200)
+
+@app.route('/branch')
+def branch(): # TODO Write this endpoint (branch)
+    return Response('Default Response', 200)
+
+if __name__ == "__main__":
+    app.run(port=8080)
--- bored-api/info.md
+++ bored-api/info.md
@@ -1,1 +1,34 @@
-Info on this section
+# Bored API
+
+The bored API will be called when the following actions are taken with the following information granted. All requests will have a token related to the project it is for as well.
+
+* On Review
+    * Branch Title
+    * Diff
+* On Merge
+    * Branch Title
+    * Diff
+* On PR
+    * Branch Title
+    * Diff
+* On Branch Creation
+    * Branch Title
+    * Branch From
+
+## Endpoints
+
+### /review
+
+Messaged when review is started
+
+### /merge
+
+Messaged when two branches are merged
+
+### /pr
+
+Messaged when a pull request is made
+
+### /branch
+
+Messaged on branch creation
