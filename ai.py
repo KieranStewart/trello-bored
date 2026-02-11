efff8a7bd1d3c2d8f9629e071b8370dd8b06811e
atauln <ataulnoor75@gmail.com>
2026-02-11 16:24:53 -0500

    ai templating complete

---
+++ ai.py
@@ -0,0 +1,32 @@
+import asyncio
+from llama_index.llms.google_genai import GoogleGenAI
+from llama_index.core.agent.workflow import FunctionAgent
+from dotenv import load_dotenv
+import os
+from tools import get_tools
+
+load_dotenv()
+
+system_prompt = """
+You are a helpful assistant that interacts with GitHub issues and sends simple responses.
+Your job is to use the tools provided to you to get information about GitHub issues and their statuses.
+You have access to the following tools:
+"""
+
+for tool in get_tools():
+    system_prompt += f"- {tool.__name__}\n"
+
+agent = FunctionAgent(
+    llm=GoogleGenAI(api_key=os.getenv("GEMINI_API_KEY"), model="gemini-2.5-flash"),
+    tools=get_tools(),
+)
+
+async def main():
+    user_input = "What are the current open issues on GitHub?"
+    response = await agent.run(system_prompt + "\n" + user_input)
+    print(response)
+    # print thinking
+    print(response)
+
+if __name__ == "__main__":
+    asyncio.run(main())
