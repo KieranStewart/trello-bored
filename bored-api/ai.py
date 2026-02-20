47104f476d2f09a5e4f69f83238b213d089de0d8
atauln <ataulnoor75@gmail.com>
2026-02-20 14:24:53 -0500

    standardized tools source

--- bored-api/ai.py
+++ bored-api/ai.py
@@ -4,29 +4,13 @@ from llama_index.core.agent.workflow import FunctionAgent
 from bored_api import pr
 from dotenv import load_dotenv
 import os
-from tools import get_tools
 from proxy_mgmt.proxy import BoardProxy
 
 load_dotenv()
 
-system_prompt = """
-You are a helpful assistant that interacts with GitHub issues and sends simple responses.
-Your job is to use the tools provided to you to get information about GitHub issues and their statuses.
-You have access to the following tools:
-"""
-
-for tool in get_tools():
-    system_prompt += f"- {tool.__name__}\n"
-
-async def main():
-    agent = FunctionAgent(
-        llm=GoogleGenAI(api_key=os.getenv("GEMINI_API_KEY"), model="gemini-2.5-flash"),
-        tools=get_tools(),
-        system_prompt=system_prompt,
-    )
-    user_input = "What are the current open issues on GitHub?"
-    response = await agent.run(user_input)
-    print(response)
+def get_tools():
+    proxy = BoardProxy()
+    return proxy.get_all_tools()
 
 async def query_agent(tools: list[function], system_prompt: str, user_input: str):
     agent = FunctionAgent(
