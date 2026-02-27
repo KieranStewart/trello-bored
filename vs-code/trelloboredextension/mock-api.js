671bdd90dba2ae475d8dd7dd576e406977dda64f
AJBruno23 <ajbru23@gmail.com>
2026-02-27 14:15:20 -0500

    updated view of approve page

--- vs-code/trelloboredextension/mock-api.js
+++ vs-code/trelloboredextension/mock-api.js
@@ -2,21 +2,25 @@ const http = require('http');
 
 const mockData = [
     {
+        name: "Test ticket 1",
         type: "Card Created",
         description: "New card 'Implement login feature' added to 'To Do' list",
         timestamp: "2024-01-15 10:30 AM"
     },
     {
+        name: "Test ticket 2",
         type: "Card Moved",
         description: "Card 'Fix bug #123' moved from 'In Progress' to 'Done'",
         timestamp: "2024-01-15 11:45 AM"
     },
     {
+        name: "Test ticket 3",
         type: "Card Updated",
         description: "Card 'Design homepage' description updated",
         timestamp: "2024-01-15 02:15 PM"
     },
     {
+        name: "Test ticket 4",
         type: "Card Deleted",
         description: "Card 'Old task' removed from 'Backlog'",
         timestamp: "2024-01-15 03:30 PM"
